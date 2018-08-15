1d1d0e65c7f108044ea036480011aa18fda32792
Junseo Bang <junseo1020@gmail.com>
2018-08-15 21:48:33 +0900

    Update Main Layout

--- App.js
+++ App.js
@@ -7,43 +7,113 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, Alert, TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+    	<View style={styles.main}>
+    		<Text style={styles.title}>
+				welcome.
+			</Text>
+			<Text style={styles.content}>
+				지금 바로 나의 mintboard와{"\n"}나의 디바이스를 연결할 수 있습니다.
+			</Text>
+
+			<LinearGradient
+          		colors={['#5ed1b7', '#0ddf7f']}
+          		start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
+          		style={styles.bottomView}
+        		>
+			</LinearGradient>
+
+			<View style={styles.bottomView}>
+			</View>
+
+			<TouchableOpacity
+   style={{
+       borderWidth:1,
+       borderColor:'#4b4b4b',
+       alignItems:'center',
+       justifyContent:'center',
+       width:64,
+       height:64,
+       backgroundColor:'#4b4b4b',
+       borderRadius:100,
+       position: 'absolute',
+       bottom: 24,
+       right: 24
+     }}
+     onPress={() => {
+    Alert.alert('You tapped the button!');
+  }}
+ >
+   {/* <Icon name={"chevron-right"}  size={30} color="#01a699" /> */}
+ </TouchableOpacity>
+
+    	</View>
     );
   }
 }
 
+SampleFunction = () => {
+	Alert.alert("Test Alert");
+}
+
 const styles = StyleSheet.create({
+  main: {
+  	flex: 1,
+  	paddingTop: 40,
+  	paddingLeft: 24,
+  	paddingRight: 24
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fff'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  title: {
+  	fontSize: 40,
+  	fontWeight: 'bold',
+  	color: '#4a4a4a',
+  	fontFamily: "NotoSansKR-Regular"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  content: {
+  	color: '#9b9b9b',
+  	fontSize: 15,
+  	textAlign: 'left',
+  	marginTop: 7,
+  	fontFamily: "NotoSansKR-Regular"
   },
+  bottomView:{
+    width: '200%', 
+    height: 270,
+    transform: [{ rotate: '25deg'}],
+    position: 'absolute',
+    bottom: -180,
+    left: -100,
+    },
 });
+
+const actions = [{
+    text: 'Accessibility',
+    //icon: require('./images/ic_accessibility_white.png'),
+    name: 'bt_accessibility',
+    position: 2
+  }, {
+    text: 'Language',
+    //icon: require('./images/ic_language_white.png'),
+    name: 'bt_language',
+    position: 1
+  }, {
+    text: 'Location',
+    //icon: require('./images/ic_room_white.png'),
+    name: 'bt_room',
+    position: 3
+  }, {
+    text: 'Video',
+    //icon: require('./images/ic_videocam_white.png'),
+    name: 'bt_videocam',
+    position: 4
+  }];
---
+++ Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class Main extends Component<Props> {
+	render() {
+		return(
+			<View>
+				<Text>asdasd</Text>
+			</View>
+		);
+	}
+}
