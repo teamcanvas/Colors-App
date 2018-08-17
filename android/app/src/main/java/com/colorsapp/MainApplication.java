c41e9c519104dfbf2df7942711e69b7b636f290f
Junseo Bang <junseo1020@gmail.com>
2018-08-17 21:17:24 +0900

    Arrange Layout and Add BLE Scan

--- android/app/src/main/java/com/colorsapp/MainApplication.java
+++ android/app/src/main/java/com/colorsapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.colorsapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import it.innove.BleManagerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BleManagerPackage(),
             new LinearGradientPackage()
       );
     }
